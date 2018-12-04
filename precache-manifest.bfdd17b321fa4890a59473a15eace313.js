7d74fe300f1dd36cd74bd1e8afe245ce6ff72f00
fzorzi <44196185+fzorzi@users.noreply.github.com>
2018-12-04 17:54:53 +0100

    Updates

---
+++ precache-manifest.bfdd17b321fa4890a59473a15eace313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b829f4d87cb33d808a78",
+    "url": "/static/js/main.b829f4d8.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "b829f4d87cb33d808a78",
+    "url": "/static/css/main.0e73338e.chunk.css"
+  },
+  {
+    "revision": "7650e9f8275c2112e78f825948b7a6b3",
+    "url": "/index.html"
+  }
+];
